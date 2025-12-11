660986db784c35f4866c46edb9241a4193c4e8ff
n4ver <nicholaslkchng@gmail.com>
2025-12-11 08:55:13 +0800

    Remove auth and DB, repurposing

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -1,9 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 
-db = SQLAlchemy()
-login_manager = LoginManager()
 
 
 def create_app():
@@ -11,11 +7,7 @@ def create_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
 
-    db.init_app(app)
-    login_manager.init_app(app)
-
     with app.app_context():
         from . import routes  # Import routes
-        db.create_all()  # Create sql tables for our data models
 
         return app
--- dashboard/routes.py
+++ dashboard/routes.py
@@ -1,11 +1,7 @@
 from flask import render_template, request, redirect, url_for
 from flask import current_app as app
-from flask_login import current_user, login_user
 
 from .common import extract_log_no, special_sort
-from .forms import SignupForm, LoginForm
-from .models import User
-from . import db, login_manager
 
 import requests
 import json
@@ -14,42 +10,6 @@ import time
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))  
-    
-    form = SignupForm()
-    if form.validate_on_submit():
-        existing_user = User.query.filter_by(email=form.email.data).first()
-        if existing_user is None:
-            user = User(
-                username=form.name.data,
-                email=form.email.data
-            )
-            user.set_password(form.password.data)
-            db.session.add(user)
-            db.session.commit()  # Create new user
-            login_user(user)  # Log in as newly created user
-    return render_template('register.html', form=form, title='Create an Account.')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))  
-
-    form = LoginForm()
-    # Validate login attempt
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()  
-        if user and user.check_password(password=form.password.data):
-            login_user(user)
-            next_page = request.args.get('next')
-            return redirect(next_page or url_for('index'))
-    return render_template('login.html', form=form, title='Log in.')
 
 
 @app.route('/log', methods=['GET', 'POST'])
@@ -148,11 +108,3 @@ def load_json():
         return data
     except FileNotFoundError:
         return {}
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    """Check if user is logged-in upon page load."""
-    if user_id is not None:
-        return User.query.get(user_id)
-    return None
